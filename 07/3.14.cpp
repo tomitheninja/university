8e4d46bd7fe9125e583d3e18d3225742164aafdc
Südi Tamás <suditomi@pm.me>
2020-11-11 17:50:21 +0100

    Fix 07/3.14

--- 07/3.14.cpp
+++ 07/3.14.cpp
@@ -3,35 +3,19 @@
 
 using namespace std;
 
-double dot(const vector<double> &v1, const vector<double> &v2)
-{
-    double sum = 0;
-    for (size_t i = 0; i < v1.size(); i++)
-    {
-        sum += v1[i] * v2[i];
-    }
-    return sum;
-}
-
 int main()
 {
     int n;
     cin >> n;
-    vector<double> v1;
+    double pro = 0;
+
     for (int i = 0; i < n; i++)
     {
-        double x;
-        cin >> x;
-        v1.push_back(x);
+        double a, b;
+        cin >> a >> b;
+        pro += a * b;
     }
 
-    vector<double> v2;
-    for (int i = 0; i < n; i++)
-    {
-        double x;
-        cin >> x;
-        v2.push_back(x);
-    }
-    cout << dot(v1, v2) << endl;
+    cout << pro << endl;
     return 0;
 }
