7a8d50cbf141fbf93f348dc0473d3040219c95bd
Südi Tamás <suditomi@pm.me>
2020-11-11 17:42:48 +0100

    fix cast error

--- 07/3.14.cpp
+++ 07/3.14.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-double dot(vector<double> &v1, vector<double> &v2)
+double dot(const vector<double> &v1, const vector<double> &v2)
 {
     double sum = 0;
-    for (int i = 0; i < v1.size(); i++)
+    for (size_t i = 0; i < v1.size(); i++)
     {
         sum += v1[i] * v2[i];
     }
