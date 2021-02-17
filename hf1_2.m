7a34fc88cc64dff3fc69712db8fe06e2159ec2ff
Südi Tamás <suditomi@pm.me>
2021-02-17 20:48:46 +0100

    Delete hf1_2.m

--- hf1_2.m
+++
@@ -1,5 +0,0 @@
-harom_hibas = nchoosek(1000, 3) * 0.001^3 * 0.999^997;
-hanyfele = nchoosek(1000, 2);
-ketto_500_888 = 1/hanyfele;
-ketto_hibas = nchoosek(1000, 2) * 0.001^2 * 0.999^998;
-szomszedos = 999/nchoosek(1000, 2) * ketto_hibas;
