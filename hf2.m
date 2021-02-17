78e8885bf2a32aec07acd86719086c662f4d1b97
Südi Tamás <suditomi@pm.me>
2021-02-17 23:40:42 +0100

    Update hf2.m

--- hf2.m
+++ hf2.m
@@ -26,3 +26,32 @@ function [B,C,d,e]=myFunction(A)
     e = mean(mins_of_col);
     
 end
+
+% works most of the time
+function [min_tav, legkozelebb, max_tav, legtavolabb, atlag_tav, kozel, utvonal] = varos_tavok(tavolsag, varos, x, utvonalhoz)
+    A2 = tavolsag;
+    A2(A2 == 0) = Inf;
+    
+    [min_tav, min_city1_idx]=min(min(A2));
+    from_min = A2(:, min_city1_idx);
+    [~, min_city2_idx]=min(from_min);
+    legkozelebb = sort([ varos(min_city1_idx) varos(min_city2_idx) ]);
+    
+    [max_tav, max_city1_idx]=max(max(tavolsag));
+    from_max = tavolsag(:, max_city1_idx);
+    [~, max_city2_idx]=max(from_max);
+    legtavolabb = sort([ varos(max_city1_idx) varos(max_city2_idx) ]);
+    atlag_tav=sum(sum(tavolsag))/(18*19);
+    dist_from_bp = tavolsag(1, :);
+    kozel = varos(dist_from_bp < x);
+    kozel = kozel(2:end);
+    kozel = sort(kozel);
+    
+    % fuck ind2sub
+    % all my homies use matrix multiplication
+    path = cat(1, utvonalhoz, [utvonalhoz(2:end) utvonalhoz(end)])';
+    path = path(1:end-1, :);
+    idx_of_routes = path * [19;1] - 19;
+    distances = tavolsag(idx_of_routes);
+    utvonal=sum(distances);
+end
