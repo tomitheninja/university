1ede69dd57448a430138a898f1340cff51ae8529
Südi Tamás <suditomi@pm.me>
2021-02-27 21:24:19 +0100

    Update hf_3.m

--- hf_3.m
+++ hf_3.m
@@ -12,30 +12,30 @@ ylabel('y value');
 p.Marker = 'square';
 p.Color = 'blue';
 p.LineStyle = '--';
+
 % plot 2
 fun2 = sin(t).*pi.^(0.1*t-3);
 subplot(2, 2, 3);
-p = plot(t, fun2);
+plot(t, fun2, '-^black');
+% line style = -
+% marker = ^
+% color = black
 xlim([t(1), t(end)]);
 ylim([-0.2, 0.2]);
 title('lowerLeft', 'FontWeight', 'bold');
 xlabel('t');
 ylabel('y value');
-p.Marker = '^';
-p.Color = 'black';
+
 % plot 3
 fun3 = cos(t).*exp(-sin(t));
 subplot(2, 2, [2 4]);
-p = plot(t, fun3, '-' ,t, t.*0);
-ax = gca; % enable grid
-ax.XGrid = 'on';
-ax.YGrid = 'on';
+plot(t, fun3, '-*red');
+yline(0, '--black'); % line at y=0
+% line style = '--'
+% line color black
+grid on;
 xlim([t(1), t(end)]);
 ylim([-1.5, 1.5]);
 title('right', 'FontWeight', 'bold');
 xlabel('t');
 ylabel('y value');
-p(1).Marker = '*';
-p(1).Color = 'red';
-p(2).Color = 'black';
-p(2).LineStyle = '--';
